9289bd3279fa9d7be57ddcdc5fee0a294177ff96
tomasitobambito <tomgacek0604@gmail.com>
2022-04-24 03:22:25 +0200

    added scalable command implemenation

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Chatbot for the twitch channel [tomasitobambito](twitch.tv/tomasitobambito).
 1. Clone the repo
 2. Enter the correct details in .env (channel name, oauth token and bot name)
 3. run `npm install tmi.js`
-4. run the bot with `node bot.js`
+4. run `npm install npm install dotenv --save`
+5. run the bot with `node bot.js`
 
 ## Features
 <img src="https://cdn.betterttv.net/emote/5d7eefb7c0652668c9e4d394/1x">
---
+++ bot.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const fs = require('fs');
+const tmi = require('tmi.js');
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = new Map();
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.set(command.name, command);
+}
+
+const opts = {
+    identity: {
+        username: process.env.NAME,
+        password: process.env.OAUTH
+    },
+    channels: [
+        process.env.CHANNEL
+    ]
+}
+
+const client = new tmi.client(opts);
+
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+client.connect();
+
+function onMessageHandler (target, ctxt, msg, self) {
+    if (self || msg[0] !== "!") { return; }
+
+    const commandName = msg.trim();
+    console.log(commandName);
+
+    if (!commands.has(commandName.substring(1))) {
+        console.log("command not found");
+        return;
+    }
+
+    try {
+        commands.get(commandName.substring(1)).execute(client, target, ctxt, msg);
+        console.log("Executed Command Succesfully");
+    } catch(Error) {
+        console.error(Error);
+    }
+}
+
+function onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+}
